baf71177106abb29e73e918480ef82f2a84102d4
Love F <lovef.code@gmail.com>
2019-03-17 11:29:12 +0100

    maven publish

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,10 +2,12 @@ plugins {
     id("se.lovef.git-version") version "0.3.3"
 }
 
+group = "se.lovef"
 version = gitVersion("0.0")
 
 println("version: $version")
 
 subprojects.forEach {
     it.version = version
+    it.group = group
 }
